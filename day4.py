18e21a02d6ad947e7f9def707edbcd400d683c2c
rshaffett <richardshaffett@me.com>
2022-12-05 15:12:38 -0600

    - Solved Day 4 Puzzle (pt1 & pt2) - Adding new file for Day 5

--- day4.py
+++ day4.py
@@ -2,13 +2,27 @@ import sys
 
 
 def readText(filename):
-
+    total = 0
+    total2 = 0
     data = open(filename, 'r')
     for line in data.readlines():
         if line[0] != '\n':
-           print("test")
+            pair1, pair2 = line.split(',')
+            a, b = pair1.split('-')
+            c, d = pair2.split('-')
+            a, b, c, d = int(a), int(b), int(c), int(d)
+            result1 = list(range(a, b + 1))
+            result2 = list(range(c, d + 1))
+            if all(value in result1 for value in result2):
+                total += 1
+            elif all(value in result2 for value in result1):
+                total += 1
+            if any(value in result1 for value in result2):
+                total2 += 1
         else:
             continue
+    print("The answer for the first part is: ", total)
+    print("The answer for the second part is: ", total + total2)
 
 
 if len(sys.argv) == 2:
